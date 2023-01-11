fc3b30b78ca3922df13431cb9ebc2bdfd552022f
EnderKill98 <no@mail.tld>
2023-01-11 05:01:52 +0100

    Add various improvements and adjustments to Player
    Some changes are done for personal reason (to incoperate with a custom
    automation mod), some improve performance on most servers or the client.
    
    Attempt at summarizing:
     - Use server limit for interact reach to increase possible usable noteblocks.
     - Put playback into separate thread. This way tps can be higher for playback
       and it prevents low or inconsistent FPS from messing up timings. Ticks
       are done on time passed instead of assumed ideal clientside 20 TPS.
       The accuracy is up to debate and should probably be configurable.
     - Missed notes should now be caught up to (mostly) to prevent skipped notes.
       The threshold of when to drop notes is arbitrary and there is surely
       a more educated value.
     - Send break packet instead of attackBlock to prevent issues in async thread
     - Additionally send ABORT_BLOCK_BREAK. This prevents visual breaking for other
       players.
     - Only send look and swing packets roughly every server tick. This reduces
       packets. I consider them wasted cosmetic packets, but some anticheats
       might make this a poor choice (though using this on servers with anticheat
       is probably a bad idea anyway).
     - Disable cosmetic packets (cancel, swing, look) when a lot of packets are
       being sent. This should prevent reaching the packet cap. Only the necessary
       packet is used then to keep the song playing. If we get very close to the
       server packet limit, packets are temporarily disabled outright to prevent
       getting kicked. Songs like Rush E can achieve this otherwise.
     - Adjusted tuning amount/delay to make it fit with server side limits afaik.
     - Disable visual swinging for client by sending the packet. Otherwise race
       conitions could happen since the playback thread is not sync.
    
    I aplologize for doing a commit for so many changes, but given that I modified
    and tested quite a lot, it was hard to split the changes into multiple commits
    later for now.
    
    Tuning is kept on normal ticks to prevent race conditions. Using
    client.submitAndJoin() from the playback thread turned out to
    somehow deadlock everything.
    
    P.S.:
    Feel free to use partial changes. No credit needed. This mod is really
    amazing and I'm sorry that my haphazard changes and adjustments for
    personal integrations make this impossible to integrate as is.
    
    I also don't use 1.19.3 yet so I couldn't base it on the most recent
    version sadly.
    
    Kudos for making this mod! Have been searching for something like
    this for a while and didn't find anything until now. It seemed only
    hack clients had these kinds of players which made it unusable for me
    since I don't want to use existing hack clients at all (no offence to them).

--- src/main/java/semmiedev/disc_jockey/SongPlayer.java
+++ src/main/java/semmiedev/disc_jockey/SongPlayer.java
@@ -9,6 +9,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
@@ -22,7 +23,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SongPlayer implements ClientTickEvents.StartWorldTick {
-    private static final Box BOX = new Box(0, 0, 0, 1, 1, 1);
     private static boolean warned;
 
     public boolean running;
@@ -32,9 +32,47 @@ public class SongPlayer implements ClientTickEvents.StartWorldTick {
     private float tick;
     private HashMap<Instrument, HashMap<Byte, BlockPos>> noteBlocks = null;
     private boolean tuned;
-    private int tuneDelay = 5;
+    private long tuneDelayUntil = -1L;
+    private long lastPlaybackTickAt = -1L;
 
-    public void start(Song song) {
+    // Used to check and enforce packet rate limits to not get kicked
+    private long last100MsSpanAt = -1L;
+    private int last100MsSpanEstimatedPackets = 0;
+    // At how many packets/100ms should the player just reduce / stop sending packets for a while
+    final private int last100MsReducePacketsAfter = 300 / 10, last100MsStopPacketsAfter = 450 / 10;
+    // If higher than current millis, don't send any packets of this kind (temp disable)
+    private long reducePacketsUntil = -1L, stopPacketsUntil = -1L;
+
+    // Use to limit swings and look to only each tick. More will not be visually visible anyway due to interpolation
+    private long lastLookSentAt = -1L, lastSwingSentAt = -1L;
+
+    // The thread executing the tickPlayback method
+    private Thread playbackThread = null;
+    public long playbackLoopDelay = 5;
+    // Just for external debugging purposes
+    public HashMap<Block, Integer> missingInstrumentBlocks = new HashMap<>();
+
+    public synchronized void startPlaybackThread() {
+        this.playbackThread = new Thread(() -> {
+            Thread ownThread = this.playbackThread;
+            while(ownThread == this.playbackThread) {
+                try {
+                    // Accuracy doesn't really matter at this precision imo
+                    Thread.sleep(playbackLoopDelay);
+                }catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                tickPlayback();
+            }
+        });
+        this.playbackThread.start();
+    }
+
+    public synchronized void stopPlaybackThread() {
+        this.playbackThread = null; // Should stop on its own then
+    }
+
+    public synchronized void start(Song song) {
         if (!Main.config.hideWarning && !warned) {
             MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.translatable("disc_jockey.warning").formatted(Formatting.BOLD, Formatting.RED));
             warned = true;
@@ -43,26 +81,132 @@ public class SongPlayer implements ClientTickEvents.StartWorldTick {
         if (running) stop();
         this.song = song;
         Main.TICK_LISTENERS.add(this);
+        if(this.playbackThread == null) startPlaybackThread();
         running = true;
+        lastPlaybackTickAt = System.currentTimeMillis();
+        last100MsSpanAt = System.currentTimeMillis();
+        last100MsSpanEstimatedPackets = 0;
+        reducePacketsUntil = -1L;
+        stopPacketsUntil = -1L;
+        lastLookSentAt = -1L;
+        lastSwingSentAt = -1L;
+        missingInstrumentBlocks.clear();
     }
 
-    public void stop() {
+    public synchronized void stop() {
         MinecraftClient.getInstance().send(() -> Main.TICK_LISTENERS.remove(this));
+        stopPlaybackThread();
         running = false;
         index = 0;
         tick = 0;
         noteBlocks = null;
         tuned = false;
+        lastPlaybackTickAt = -1L;
+        last100MsSpanAt = -1L;
+        last100MsSpanEstimatedPackets = 0;
+        reducePacketsUntil = -1L;
+        stopPacketsUntil = -1L;
+        lastLookSentAt = -1L;
+        lastSwingSentAt = -1L;
+    }
+
+    public synchronized void tickPlayback() {
+        if (!running) {
+            lastPlaybackTickAt = -1L;
+            last100MsSpanAt = -1L;
+            return;
+        }
+        long previousPlaybackTickAt = lastPlaybackTickAt;
+        lastPlaybackTickAt = System.currentTimeMillis();
+        if(last100MsSpanAt != -1L && System.currentTimeMillis() - last100MsSpanAt >= 100) {
+            last100MsSpanEstimatedPackets = 0;
+            last100MsSpanAt = System.currentTimeMillis();
+        }else if (last100MsSpanAt == -1L) {
+            last100MsSpanAt = System.currentTimeMillis();
+            last100MsSpanEstimatedPackets = 0;
+        }
+        if(noteBlocks != null && tuned) {
+            while (running) {
+                MinecraftClient client = MinecraftClient.getInstance();
+                GameMode gameMode = client.interactionManager.getCurrentGameMode();
+                if (!gameMode.isSurvivalLike()) {
+                    client.inGameHud.getChatHud().addMessage(Text.translatable(Main.MOD_ID+".player.invalid_game_mode", gameMode.getTranslatableName()).formatted(Formatting.RED));
+                    stop();
+                    return;
+                }
+
+                long note = song.notes[index];
+                final long now = System.currentTimeMillis();
+                if ((short)note <= Math.round(tick)) {
+                    BlockPos blockPos = noteBlocks.get(Note.INSTRUMENTS[(byte)(note >> Note.INSTRUMENT_SHIFT)]).get((byte)(note >> Note.NOTE_SHIFT));
+                    if (!canInteractWith(client.player, blockPos)) {
+                        stop();
+                        client.inGameHud.getChatHud().addMessage(Text.translatable(Main.MOD_ID+".player.to_far").formatted(Formatting.RED));
+                        return;
+                    }
+                    Vec3d unit = Vec3d.ofCenter(blockPos, 0.5).subtract(client.player.getEyePos()).normalize();
+                    if((lastLookSentAt == -1L || now - lastLookSentAt >= 50) && last100MsSpanEstimatedPackets < last100MsReducePacketsAfter && (reducePacketsUntil == -1L || reducePacketsUntil < now)) {
+                        client.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(MathHelper.wrapDegrees((float) (MathHelper.atan2(unit.z, unit.x) * 57.2957763671875) - 90.0f), MathHelper.wrapDegrees((float) (-(MathHelper.atan2(unit.y, Math.sqrt(unit.x * unit.x + unit.z * unit.z)) * 57.2957763671875))), true));
+                        last100MsSpanEstimatedPackets++;
+                        lastLookSentAt = now;
+                    }else if(last100MsSpanEstimatedPackets >= last100MsReducePacketsAfter){
+                        reducePacketsUntil = Math.max(reducePacketsUntil, now + 500);
+                    }
+                    if(last100MsSpanEstimatedPackets < last100MsStopPacketsAfter && (stopPacketsUntil == -1L || stopPacketsUntil < now)) {
+                        // TODO: 5/30/2022 Check if the block needs tuning
+                        //client.interactionManager.attackBlock(blockPos, Direction.UP);
+                        client.player.networkHandler.sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.START_DESTROY_BLOCK, blockPos, Direction.UP, 0));
+                        last100MsSpanEstimatedPackets++;
+                    }else if(last100MsSpanEstimatedPackets >= last100MsStopPacketsAfter) {
+                        Main.LOGGER.info("Stopping all packets for a bit!");
+                        stopPacketsUntil = Math.max(stopPacketsUntil, now + 250);
+                        reducePacketsUntil = Math.max(reducePacketsUntil, now + 10000);
+                    }
+                    if(last100MsSpanEstimatedPackets < last100MsReducePacketsAfter && (reducePacketsUntil == -1L || reducePacketsUntil < now)) {
+                        client.player.networkHandler.sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.ABORT_DESTROY_BLOCK, blockPos, Direction.UP, 0));
+                        last100MsSpanEstimatedPackets++;
+                    }else if(last100MsSpanEstimatedPackets >= last100MsReducePacketsAfter){
+                        reducePacketsUntil = Math.max(reducePacketsUntil, now + 500);
+                    }
+                    if((lastSwingSentAt == -1L || now - lastSwingSentAt >= 50) &&last100MsSpanEstimatedPackets < last100MsReducePacketsAfter && (reducePacketsUntil == -1L || reducePacketsUntil < now)) {
+                        client.executeSync(() -> client.player.swingHand(Hand.MAIN_HAND));
+                        lastSwingSentAt = now;
+                        last100MsSpanEstimatedPackets++;
+                    }else if(last100MsSpanEstimatedPackets  >= last100MsReducePacketsAfter){
+                        reducePacketsUntil = Math.max(reducePacketsUntil, now + 500);
+                    }
+
+                    index++;
+                    if (index >= song.notes.length) {
+                        stop();
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            // This math has some unnecessary stuff (using bogus tps). Can be simplified a lot.
+            float tps = 1000.0F / 16.0F;
+            if (previousPlaybackTickAt != -1L && lastPlaybackTickAt != -1L) {
+                float inaccuracyFactor = (1000.0F / tps) / ((float) (lastPlaybackTickAt - previousPlaybackTickAt));
+                tps *= inaccuracyFactor;
+            }
+
+            tick += song.tempo / 100f / tps;
+        }
     }
 
     // TODO: 6/2/2022 Play note blocks every song tick, instead of every tick. That way the song will sound better
+    //      11/1/2023 Playback now done in separate thread. Not ideal but better especiall when FPS are low.
     @Override
     public void onStartTick(ClientWorld world) {
-        if (!running) return;
+
         if (noteBlocks == null) {
             noteBlocks = new HashMap<>();
 
-            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+            MinecraftClient client = MinecraftClient.getInstance();
+            ClientPlayerEntity player = client.player;
 
             ArrayList<Note> capturedNotes = new ArrayList<>();
 
@@ -71,9 +215,9 @@ public class SongPlayer implements ClientTickEvents.StartWorldTick {
                 for (int y = -7; y <= 7; y++) {
                     for (int z = -7; z <= 7; z++) {
                         BlockPos blockPos = new BlockPos(playerPos.add(x, y, z));
-                        if (intersect(playerPos, MinecraftClient.getInstance().interactionManager.getReachDistance(), BOX.offset(blockPos))) {
-                            BlockState blockState = world.getBlockState(blockPos);
-                            if (blockState.isOf(Blocks.NOTE_BLOCK) && world.isAir(blockPos.up())) {
+                        if (canInteractWith(player, blockPos)) {
+                            BlockState blockState = client.world.getBlockState(blockPos);
+                            if (blockState.isOf(Blocks.NOTE_BLOCK) && client.world.isAir(blockPos.up())) {
                                 for (Note note : song.uniqueNotes) {
                                     if (!capturedNotes.contains(note) && blockState.get(Properties.INSTRUMENT) == note.instrument) {
                                         getNotes(note.instrument).put(note.note, blockPos);
@@ -101,90 +245,53 @@ public class SongPlayer implements ClientTickEvents.StartWorldTick {
                     missing.put(block, got + 1);
                 }
 
+                missingInstrumentBlocks = missing;
                 missing.forEach((block, integer) -> chatHud.addMessage(Text.literal(block.getName().getString()+" × "+integer).formatted(Formatting.RED)));
                 stop();
             }
         } else if (!tuned) {
-            if (tuneDelay > 0) {
-                tuneDelay--;
+            if (tuneDelayUntil != -1L && tuneDelayUntil > System.currentTimeMillis()) {
                 return;
             }
             tuned = true;
+
             MinecraftClient client = MinecraftClient.getInstance();
             int tuneAmount = 0;
             for (Note note : song.uniqueNotes) {
                 BlockPos blockPos = noteBlocks.get(note.instrument).get(note.note);
-                BlockState blockState = world.getBlockState(blockPos);
+                BlockState blockState = client.world.getBlockState(blockPos);
 
                 if (blockState.contains(Properties.NOTE)) {
                     if (blockState.get(Properties.NOTE) != note.note) {
-                        if (!intersect(client.player.getEyePos(), client.interactionManager.getReachDistance(), BOX.offset(blockPos))) {
+                        if (!canInteractWith(client.player, blockPos)) {
                             stop();
                             client.inGameHud.getChatHud().addMessage(Text.translatable(Main.MOD_ID+".player.to_far").formatted(Formatting.RED));
                             return;
                         }
                         Vec3d unit = Vec3d.ofCenter(blockPos, 0.5).subtract(client.player.getEyePos()).normalize();
-                        client.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(MathHelper.wrapDegrees((float)(MathHelper.atan2(unit.z, unit.x) * 57.2957763671875) - 90.0f), MathHelper.wrapDegrees((float)(-(MathHelper.atan2(unit.y, Math.sqrt(unit.x * unit.x + unit.z * unit.z)) * 57.2957763671875))), true));
+                        client.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(MathHelper.wrapDegrees((float) (MathHelper.atan2(unit.z, unit.x) * 57.2957763671875) - 90.0f), MathHelper.wrapDegrees((float) (-(MathHelper.atan2(unit.y, Math.sqrt(unit.x * unit.x + unit.z * unit.z)) * 57.2957763671875))), true));
                         client.interactionManager.interactBlock(client.player, Hand.MAIN_HAND, new BlockHitResult(Vec3d.of(blockPos), Direction.UP, blockPos, false));
                         client.player.swingHand(Hand.MAIN_HAND);
                         tuned = false;
-                        tuneDelay = 5;
-                        if (++tuneAmount == 6) break;
+
+                        // The server (at least) paper allows 8 interacts per 300ms afaik
+                        tuneDelayUntil = System.currentTimeMillis() + 300;
+                        if (++tuneAmount == 8) break;
                     }
                 } else {
                     noteBlocks = null;
                     break;
                 }
             }
-        } else {
-            while (running) {
-                MinecraftClient client = MinecraftClient.getInstance();
-                GameMode gameMode = client.interactionManager.getCurrentGameMode();
-                if (!gameMode.isSurvivalLike()) {
-                    client.inGameHud.getChatHud().addMessage(Text.translatable(Main.MOD_ID+".player.invalid_game_mode", gameMode.getTranslatableName()).formatted(Formatting.RED));
-                    stop();
-                    return;
-                }
-
-                long note = song.notes[index];
-                if ((short)note == Math.round(tick)) {
-                    BlockPos blockPos = noteBlocks.get(Note.INSTRUMENTS[(byte)(note >> Note.INSTRUMENT_SHIFT)]).get((byte)(note >> Note.NOTE_SHIFT));
-                    if (!intersect(client.player.getEyePos(), client.interactionManager.getReachDistance(), BOX.offset(blockPos))) {
-                        stop();
-                        client.inGameHud.getChatHud().addMessage(Text.translatable(Main.MOD_ID+".player.to_far").formatted(Formatting.RED));
-                        return;
-                    }
-                    Vec3d unit = Vec3d.ofCenter(blockPos, 0.5).subtract(client.player.getEyePos()).normalize();
-                    client.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(MathHelper.wrapDegrees((float)(MathHelper.atan2(unit.z, unit.x) * 57.2957763671875) - 90.0f), MathHelper.wrapDegrees((float)(-(MathHelper.atan2(unit.y, Math.sqrt(unit.x * unit.x + unit.z * unit.z)) * 57.2957763671875))), true));
-                    // TODO: 5/30/2022 Check if the block needs tuning
-                    client.interactionManager.attackBlock(blockPos, Direction.UP);
-                    client.player.swingHand(Hand.MAIN_HAND);
-
-                    index++;
-                    if (index >= song.notes.length) {
-                        stop();
-                        break;
-                    }
-                } else {
-                    break;
-                }
-            }
-
-            tick += song.tempo / 100f / 20f;
         }
-    }
-
-    private boolean intersect(Vec3d pos, double radius, Box box) {
-        double x = Math.max(box.minX, Math.min(pos.x, box.maxX));
-        double y = Math.max(box.minY, Math.min(pos.y, box.maxY));
-        double z = Math.max(box.minZ, Math.min(pos.z, box.maxZ));
-
-        double distance = (x - pos.x) * (x - pos.x) + (y - pos.y) * (y - pos.y) + (z - pos.z) * (z - pos.z);
-
-        return distance < radius * radius;
     }
 
     private HashMap<Byte, BlockPos> getNotes(Instrument instrument) {
         return noteBlocks.computeIfAbsent(instrument, k -> new HashMap<>());
+    }
+
+    // The server limits interacts to 6 Blocks from Player Eye to Block Center
+    private boolean canInteractWith(ClientPlayerEntity player, BlockPos blockPos) {
+        return player.getEyePos().squaredDistanceTo(new Vec3d(blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5)) <= 6.0*6.0;
     }
 }
